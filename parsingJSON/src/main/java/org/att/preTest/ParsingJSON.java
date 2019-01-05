2c709facd4b8168df5beea83ff357838902bd2da
neelima <sures@192.168.0.18>
2019-01-05 10:57:01 -0800

    Code modified with HttpURLConnection instance

--- parsingJSON/src/main/java/org/att/preTest/ParsingJSON.java
+++ parsingJSON/src/main/java/org/att/preTest/ParsingJSON.java
@@ -8,6 +8,7 @@ import java.util.Properties;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 import java.net.URL;
 
 import org.json.simple.JSONArray;
@@ -19,6 +20,7 @@ public class ParsingJSON {
 	public static final String httpURLParam = "http-URL"; //property file parameter name
 	public static final String NUMBERS = "numbers";
 	public static String propFileName = "config.properties";
+	private static final String USER_AGENT = "Mozilla/5.0";
 
 	public static void main(String[] args) {
 		String httpURLGet = null;
@@ -68,15 +70,29 @@ public class ParsingJSON {
 	    StringBuilder sb = new StringBuilder();
 	    JSONArray JSONData = new JSONArray();
 	    try {
-	        URL url = new URL(httpURLGet);
-	        BufferedReader rd = new BufferedReader(new InputStreamReader(url.openStream()));
-	        String st;
-			while ( (st = rd.readLine().toString()) != null) {
-				sb.append(st);
+	    	URL obj = new URL(httpURLGet);
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+			con.setRequestMethod("GET");
+			con.setRequestProperty("User-Agent", USER_AGENT);
+			int responseCode = con.getResponseCode();
+		//	System.out.println("GET Response Code :: " + responseCode);
+			if (responseCode == HttpURLConnection.HTTP_OK) { // success
+				BufferedReader in = new BufferedReader(new InputStreamReader(
+						con.getInputStream()));
+				String inputLine;
+				StringBuffer response = new StringBuffer();
+	
+				while ((inputLine = in.readLine()) != null) {
+					response.append(inputLine);
+				}
+				in.close();
+		//		System.out.println(response.toString());
+				JSONParser parser = new JSONParser();
+				JSONObject json = (JSONObject) parser.parse(response.toString());
+				JSONData.add(json);
+			} else {
+				System.out.println("GET request not worked");
 			}
-			JSONParser parser = new JSONParser();
-			JSONObject json = (JSONObject) parser.parse(sb.toString());
-			JSONData.add(json);
 	    }catch (ParseException e) {
 			System.out.println(e.getMessage());
 		}catch(IOException e){
@@ -98,11 +114,12 @@ public class ParsingJSON {
 	        Iterator nums = numArray.iterator(); 
 	        while (nums.hasNext())  
 	        { 
-	        	arraySum += (Integer) nums.next();
+	        	arraySum += Integer.parseInt(nums.next().toString());
+	        	
 	        }
 		    grandTotal += arraySum;
 		  }
-		  System.out.println("sum of number array in a given JSON file is :"+grandTotal);
+		  System.out.println("sum of the number array in a given JSON file is :"+grandTotal);
 		}
 		catch(Exception e) {
 			System.out.println("unable to parse a given JSON :"+e.getMessage());
